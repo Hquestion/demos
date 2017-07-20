130d0cb0026a6d4340968e980f70a5a1ee962b04
Hquestion <1035271660@qq.com>
2017-07-20 17:42:12 +0800

    进度条插件绘制动画和demo

--- dragsort/jquery.dragsort.js
+++ dragsort/jquery.dragsort.js
@@ -2,9 +2,82 @@
  * Created by 贺小雷 on 2017-07-19.
  */
 (function(window, $){
+	var throttle = function ( delay, noTrailing, callback, debounceMode ) {
+
+		// After wrapper has stopped being called, this timeout ensures that
+		// `callback` is executed at the proper times in `throttle` and `end`
+		// debounce modes.
+		var timeoutID;
+
+		// Keep track of the last time `callback` was executed.
+		var lastExec = 0;
+
+		// `noTrailing` defaults to falsy.
+		if ( typeof noTrailing !== 'boolean' ) {
+			debounceMode = callback;
+			callback = noTrailing;
+			noTrailing = undefined;
+		}
+
+		// The `wrapper` function encapsulates all of the throttling / debouncing
+		// functionality and when executed will limit the rate at which `callback`
+		// is executed.
+		function wrapper () {
+
+			var self = this;
+			var elapsed = Number(new Date()) - lastExec;
+			var args = arguments;
+
+			// Execute `callback` and update the `lastExec` timestamp.
+			function exec () {
+				lastExec = Number(new Date());
+				callback.apply(self, args);
+			}
+
+			// If `debounceMode` is true (at begin) this is used to clear the flag
+			// to allow future `callback` executions.
+			function clear () {
+				timeoutID = undefined;
+			}
+
+			if ( debounceMode && !timeoutID ) {
+				// Since `wrapper` is being called for the first time and
+				// `debounceMode` is true (at begin), execute `callback`.
+				exec();
+			}
+
+			// Clear any existing timeout.
+			if ( timeoutID ) {
+				clearTimeout(timeoutID);
+			}
+
+			if ( debounceMode === undefined && elapsed > delay ) {
+				// In throttle mode, if `delay` time has been exceeded, execute
+				// `callback`.
+				exec();
+
+			} else if ( noTrailing !== true ) {
+				// In trailing throttle mode, since `delay` time has not been
+				// exceeded, schedule `callback` to execute `delay` ms after most
+				// recent execution.
+				//
+				// If `debounceMode` is true (at begin), schedule `clear` to execute
+				// after `delay` ms.
+				//
+				// If `debounceMode` is false (at end), schedule `callback` to
+				// execute after `delay` ms.
+				timeoutID = setTimeout(debounceMode ? clear : exec, debounceMode === undefined ? delay - elapsed : delay);
+			}
+
+		}
+
+		// Return the wrapper function.
+		return wrapper;
+
+	};
 	$.fn.dragsort = function(option){
 		var defaults = {
-			ele: 'li',
+			ele: '.item',
 			container: 'ul',
 			handler: '.handler',
 			dragstartCb: $.noop(),
@@ -13,7 +86,7 @@
 		};
 		$.extend(defaults, option);
 
-		var $container = $(this);
+		var $container;
 
 		var isDragging = false;
 		var $dragItem = null;
@@ -25,28 +98,81 @@
 			dy = e.clientY;
 
 			var $item = $dragItem = $(this).parents(defaults.ele);
+			$container = $item.parents(defaults.container);
 			originalOffset = $item.offset();
 			$item.addClass('is-dragging');
 			isDragging = true;
-			$item.on('dragstart', function(e){
-
+			$item.off('dragstart').on('dragstart', function(e){
+				console.error('dragStart...');
+				setTimeout(function(){
+					$item.addClass('drag-ghost');
+				}, 0);
 			});
 			//支持拖放
-			$container.on('dragover', function(e){
+			$container.off('dragover').on('dragover', function(e){
 				e.preventDefault();
+				console.log(e.target.className);
+				if(e.target.className.indexOf(defaults.ele.slice(1)) >= 0) {
+					var targetEle = $(e.target);
+					var currentOffset = targetEle.offset().top;
+					var dragEleOffset = $dragItem.offset().top;
+					var moveY = dragEleOffset - currentOffset;
+					targetEle.css({
+						transition: 'all ease .2s',
+						transform: 'translate3d(0,'+(moveY)+'px, 0)'
+					});
+					setTimeout(function(){
+						if(moveY > 0) {
+							targetEle.before($dragItem);
+						}else {
+							targetEle.after($dragItem);
+						}
+						targetEle.css({
+							transition: 'none',
+							transform: 'translate3d(0,0,0)'
+						});
+					}, 200);
+				}
 			});
-
 		});
 
 		$(window).off('mousemove.drag').on('mousemove.drag', function(e){
 			isDragging && $dragItem.attr('draggable', 'true');
 		});
 
+		$(window).off('mouseup.drag').on('mouseup.drag', function(){
+			if($dragItem) {
+				$dragItem.attr('draggable', 'false');
+				$dragItem.removeClass('is-dragging').removeClass('drag-ghost');
+				isDragging = false;
+			}
+		});
+
 		$(this).delegate(defaults.ele, 'drop', function(e){
+			console.error('do drop...');
 			e.preventDefault();
-			$(this).after($dragItem);
-			isDragging && $dragItem.attr('draggable', 'false');
+			// var currentOffset = $(this).offset().top;
+			// var dragEleOffset = $dragItem.offset().top;
+			// $(this).css({
+			// 	transition: 'all ease .2s',
+			// 	transform: 'translate3d(0,'+(dragEleOffset - currentOffset)+'px, 0)'
+			// });
+			// var targetEle = $(this);
+			// setTimeout(function(){
+			// 	targetEle.after($dragItem);
+			// 	targetEle.css({
+			// 		transition: 'none',
+			// 		transform: 'translate3d(0,0,0)'
+			// 	});
+			// 	$dragItem.attr('draggable', 'false');
+			// 	$dragItem.removeClass('is-dragging').removeClass('drag-ghost');
+			// 	isDragging = false;
+			// 	$dragItem = null;
+			// }, 200);
+			$dragItem.attr('draggable', 'false');
+			$dragItem.removeClass('is-dragging').removeClass('drag-ghost');
 			isDragging = false;
+			$dragItem = null;
 		});
 	};
 })(this, $);
